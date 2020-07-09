e25fb60644a76befc21b921f56d92153d3c2cac9
shandyk <shandyjkim@gmail.com>
2020-07-09 18:47:46 +0000

    Add chart page to portfolio

---
+++ portfolio/src/main/webapp/chart.js
@@ -0,0 +1,24 @@
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+/** Creates a chart and adds it to the page. */
+function drawChart() {
+  const data = new google.visualization.DataTable();
+  data.addColumn('string', 'Animal');
+  data.addColumn('number', 'Count');
+        data.addRows([
+          ['Working', 5],
+          ['Sleeping', 20],
+          ['Distracted', 75]
+        ]);
+
+  const options = {
+    'title': 'Productivity',
+    'width':500,
+    'height':400
+  };
+
+  const chart = new google.visualization.PieChart(
+      document.getElementById('chart-container'));
+  chart.draw(data, options);
+}
