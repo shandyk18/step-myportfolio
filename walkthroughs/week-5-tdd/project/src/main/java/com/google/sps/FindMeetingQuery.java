8c8505780ecb48912af848412e80c91f5228bb45
shandyk <shandyjkim@gmail.com>
2020-07-13 20:25:57 +0000

    Add prelim corner checks and pseudocode main algorithm

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -15,9 +15,49 @@
 package com.google.sps;
 
 import java.util.Collection;
+import java.util.ArrayList;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    Collection<TimeRange> validTimes = new ArrayList<>();
+    Collection<TimeRange> invalidTimes = new ArrayList<>();
+    Collection<String> attendees = request.getAttendees();
+    int duration = (int) request.getDuration();
+    int start = TimeRange.START_OF_DAY;
+
+    // if length of meeting is longer than a whole day, return empty list
+    if (duration > TimeRange.WHOLE_DAY.duration()) {
+        return new ArrayList<TimeRange>();
+    }
+
+    // if no conflicting events or no attendees
+    if (attendees.isEmpty() || events.isEmpty()) {
+        validTimes.add(TimeRange.WHOLE_DAY);
+        return validTimes;
+    }
+
+    // Add to invalid times collection
+    for (Event event : events) {
+        invalidTimes.add(event.getWhen());
+    }
+
+    Collections.sort(busyTimeRanges, TimeRange.ORDER_BY_START);
+
+    // find intersection between the events for easier view of invalid times
+    // for loop for start times (int start) and check if:
+      // start time and duration conflicts with start time of current invalid event
+        // if it does, break and increment start time
+      // if not, keep looping through all events and do same check
+      // keep track of end time
+        // have a variable for end time of first event, decrease end time if event overlaps with range
+        // and still fits within duration
+      // increment start time var by duration of range
+      // add range to validTimes
+
+
+    for (Event invalid : invalidTimes) {
+    }
+
+    return validTimes;
   }
 }
